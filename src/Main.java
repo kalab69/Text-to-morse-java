f4875c73db4d305f3de799bc9e0078bc40d763ff
kalab <kalabb69@gmail.com>
2026-06-14 17:56:58 +0300

    finished it added user ui with options and made it so it prints error if a user inputed a invalid Morse code with try catch block

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.util.HashMap;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-
-        // word to Morse
+    // word to Morse
+    public static String charToMorse(char x){
         HashMap<Character,String> convert = new HashMap<>();
         convert.put('0',"-----");
         convert.put('1',".----");
@@ -43,8 +41,10 @@ public class Main {
         convert.put('Y', "-.--");
         convert.put('Z', "--..");
         convert.put(' ',"/");
-
-        // Morse to word
+        return convert.get(x);
+    }
+    // Morse to word
+    public static char MorseTochar(String x){
         HashMap<String,Character> rechange = new HashMap<>();
         rechange.put("/",' ');
         rechange.put(".-", 'A');
@@ -83,36 +83,75 @@ public class Main {
         rechange.put("--...", '7');
         rechange.put("---..", '8');
         rechange.put("----.", '9');
-        System.out.println("enter Morse code to be translated");
-        String code= sc.nextLine();
-        String icode="";
-        int p=0;
-        char last[]= new char[1000];
-        for (int i = 0; i < code.length(); i++) {
+        return rechange.get(x);
+    }
 
-            if (' '==code.charAt(i)){
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String answer="";
+        do {
+            System.out.println("==========Morse to sentence and sentence to Morse===========");
+            System.out.println("1 change Morse to words\n2 change words to Morse");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice){
+                case 1:
+                    try {
+                        System.out.println("enter Morse code to be translated");
+                        String code = sc.nextLine();
+                        String icode = "";
+                        int p = 0;
+                        char last[] = new char[1000];
+                        for (int i = 0; i < code.length(); i++) {
 
-               last[p] = rechange.get(icode);
-               icode = "";
-               p++;
-            }else {
-                icode=icode+code.charAt(i);
+                            if (' ' == code.charAt(i)) {
+
+                                last[p] = MorseTochar(icode);
+                                icode = "";
+                                p++;
+                            } else {
+                                icode = icode + code.charAt(i);
+                            }
+
+                        }
+                        last[p] = MorseTochar(icode);
+
+                        System.out.println(new String(last, 0, p + 1));
+
+
+                    } catch (NullPointerException e) {
+                        System.out.println("Didn't enter a propre Morse code!!!");
+
+                    }
+                    finally {
+                        System.out.println("Do you wanna exit?");
+                        answer = sc.nextLine();
+                    }
+
+                    break;
+                case 2:
+
+                    System.out.println("enter a word and digits to be converted to Morse code");
+                    String word = sc.nextLine();
+                    String Morse = "";
+                    for (int i = 0; i < word.length(); i++) {
+                        Morse += charToMorse(word.toUpperCase().charAt(i)) + " ";
+                    }
+                    System.out.println(Morse);
+                    System.out.println("Do you wanna exit?");
+                    answer = sc.nextLine();
+
+                    break;
+                default:
+                    System.out.println("Didn't enter a proper choise (1-2) only");
+                    break;
+
             }
 
-        }
-        last[p]= rechange.get(icode);
-
-        System.out.println(new String(last,0,p+1));
 
 
-        System.out.println("enter a word and digits to be converted to Morse code");
-        String word=sc.nextLine();
-        String Morse="";
-        for (int i = 0; i < word.length(); i++) {
-            Morse += convert.get(word.toUpperCase().charAt(i))+" ";
-        }
-        System.out.println(Morse);
 
 
+        }while ( !answer.equals("yes"));
     }
 }
